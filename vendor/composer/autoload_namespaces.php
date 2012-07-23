938d1d6593999da24464d908320fea0f306eab61
Sebastian Gorecki <gorecki.sebastian@gmail.com>
2012-07-23 23:12:24 +0200

    poczatki frameworku podstawowego

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -11,5 +11,7 @@ return array(
     'Symfony\\Component\\HttpFoundation' => $vendorDir . '/symfony/http-foundation/',
     'Symfony\\Component\\EventDispatcher' => $vendorDir . '/symfony/event-dispatcher/',
     'Symfony\\Component\\ClassLoader' => $vendorDir . '/symfony/class-loader/',
+    'Simplex' => $baseDir . '/src/',
     'SessionHandlerInterface' => $vendorDir . '/symfony/http-foundation/Symfony/Component/HttpFoundation/Resources/stubs',
+    'Calendar' => $baseDir . '/src/',
 );
