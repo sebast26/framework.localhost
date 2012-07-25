eb055299e57e2facb9bd60fa502244ad2d8c3b99
Sebastian <sebast26@man.poznan.pl>
2012-07-25 14:31:48 +0200

    dodano obsluge bledow oraz przeniesiono caly framework jako HttpKernel

--- web/front.php
+++ web/front.php
@@ -7,22 +7,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\HttpCache\HttpCache;
-use Symfony\Component\HttpKernel\HttpCache\Store;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
 $dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new Simplex\GoogleListener());
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
+$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
 
-$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
-$framework = new HttpCache($framework, new Store(__DIR__.'/../cache'));
+$listener = new HttpKernel\EventListener\ExceptionListener('Calendar\\Controller\\ErrorController::exceptionAction');
+$dispatcher->addSubscriber($listener);
+
+$dispatcher->addSubscriber(new Simplex\StringResponseListener());
+
+$framework = new Simplex\Framework($dispatcher, $resolver);
 
 $framework->handle($request)->send();
